4c8164d9fd89980141d279e63b715e5afb67c776
alex <tulen.spb@mail.ru>
2021-07-05 22:05:32 +0300

    Not complete advanced truth tablet

--- src/com/company/Truth_Table.java
+++ src/com/company/Truth_Table.java
@@ -7,7 +7,7 @@ public class Truth_Table {
     final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner("& " +
+        Scanner scanner = new Scanner("&" +
                 "|" +
                 "^");
 
@@ -15,12 +15,16 @@ public class Truth_Table {
         System.out.println ("X 1 действие Y 2 действие Z");
         System.out.println (" Ведите цифру стоящею рядом с нужным действием");
         System.out.println ("вводите сначало 1 действие затем 2");
-        System.out.println ("1 = &");
-        System.out.println ("2 = |");
-        System.out.println ("3 = ^");
-        int scan = scanner.nextInt ();
-        for ()
+        System.out.println ("&");
+        System.out.println ("|");
+        System.out.println ("^");
 
-        System.out.println ("X "+s+"  ");
-    }
+        String save = "1321";
+        save = scanner.nextLine ();
+        for ( String line2 = "0123"; ;  )
+        {
+        line2 = scanner.nextLine();
+        }
+
+            }
 }
